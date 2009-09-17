136a21b0d26e02efe5c0a75131f1d07f06b4883e
Andreas Pauley <apauley+github@gmail.com>
2009-09-17 18:20:51 +0200

    Sum of Pairs: some comments/credits

--- Snippets/SumPairs/sumpairs-map.py
+++ Snippets/SumPairs/sumpairs-map.py
@@ -1,5 +1,5 @@
-from operator import add
-
 # Solution by Pieter Nagel, based on the Clojure solution
+
+from operator import add
 a = [1, 2, 3, 4, 5]
 map(add, a[1:], a[:-1])
--- Snippets/SumPairs/sumpairs.clj
+++ Snippets/SumPairs/sumpairs.clj
@@ -1,4 +1,5 @@
 ; Solution by Timothy Pratley
 ; http://groups.google.com/group/clojure/browse_thread/thread/ca3ad049fba7ee82
+
 (def a '(1 2 3 4 5))
 (map + a (rest a))

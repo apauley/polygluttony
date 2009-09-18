5fafbd73d9159ebf148d4505fda234e4a6bee2e5
Andreas Pauley <apauley+github@gmail.com>
2009-09-18 19:14:27 +0200

    Sum of Pairs: C solutions by Hardy Jonck

--- CodeSnippets/SumPairs/sumpairs.c
+++ CodeSnippets/SumPairs/sumpairs.c
@@ -4,7 +4,9 @@
 
 int main (int argc, const char * argv[]) {
    int a[] = {1,2,3,4,5};
+       int b[sizeof(a)/sizeof(int)-1];
        char x;
-       for (x=0; x<sizeof(a)/sizeof(int)-1; x++) { a[x] += a[x+1];printf("%d,",a[x]);}
+       for (x=0; x<sizeof(a)/sizeof(int)-1; x++) { b[x] = a[x]+ a[x+1];printf("%d,",b[x]);}
+
    return 0;
 }
